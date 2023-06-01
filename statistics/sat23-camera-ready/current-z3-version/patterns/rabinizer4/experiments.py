665cd2951436b24360aa70e92463db7df9a3de93
Tereza <xschwar3@gitlab.fi.muni.cz>
2023-06-01 22:30:35 +0200

    Prepared statistics generation.

---
+++ statistics/sat23-camera-ready/current-z3-version/patterns/rabinizer4/experiments.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import spot
+import os
+import sys
+
+def main(argv):
+    print("Current telatko z3 version - patterns rabinizer4")
+    telatko = "/home/xschwar3/telatko/telatko.py"
+
+    os.environ['SPOT_TMPDIR'] = './autcross'
+    os.environ['SPOT_TMPKEEP'] = '1'
+    patterns_path = './'
+    tools_order = [  'rabinizer4']
+    for tool in tools_order:
+        print(tool)
+        os.system(f"autcross --no-checks --csv=patterns-telatko-{tool}.csv -F {patterns_path}/patterns-{tool}.hoa 'autfilt %H > %O' '{telatko} -F %H -T 30 -L 1 -I -O %O' '{telatko} -F %H -T 30 -L 2 -I -O %O' '{telatko} -F %H -T 30 -L 3 -I -O %O' '{telatko} -F %H -T 30 -L 3 -I -G -O %O' > autcross/{tool}.log 2>&1")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- telatko.py
+++ telatko.py
@@ -105,10 +105,11 @@ def main(argv):
             else:
                 print_aut(auto, None, " ")
 
-
-            #if not spot.are_equivalent(origin, auto):
-            #    print("NOT EQUIVALENT!")
-            #    assert (False)
+            """
+            if not spot.are_equivalent(origin, auto):
+                print("NOT EQUIVALENT!")
+                assert (False)
+            """
 
 
         # except BaseException as err:
